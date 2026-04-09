bae4126c0fb85f7a102bbd5099f15c3e8f95d76f
Finn-Ole Kahl <finnole.kahl@icloud.com>
2026-04-09 15:13:43 +0200

    initial commit

---
+++ App/hero_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hero_challengeApp.swift
+//  hero-challenge
+//
+//  Created by Finn on 09.04.26.
+//
+
+import SwiftUI
+
+@main
+struct hero_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
